4666a53cbdb0cbe6b334a7fd3c678d5117b420f4
plaetzaw <plaetzaw@gmail.com>
2020-03-17 15:22:39 -0500

    Setting up app skeleton

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.use(require('./routes/'));
-app.use(require('./routes/blogs.js'));
-app.use(require('./routes/editblogs.js'));
+app.use(require("./routes/"));
 
 app.listen(3000, () => {
-  console.log('Listening on 3000');
+  console.log("Listening on 3000");
 });
--- routes/editblogs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/editblogs', (req, res) => {
-  res.render('editblogs.ejs');
-});
-
-module.exports = router;
