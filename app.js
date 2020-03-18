504a4ddb5f0ae8aa5d0d0a0ab5b327879654cef0
plaetzaw <plaetzaw@gmail.com>
2020-03-18 15:38:15 -0500

    Adding NavBar Changes and FoodExc page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.use(require("./routes/"));
 app.use(require("./routes/login"));
 app.use(require("./routes/register"));
+app.use(require("./routes/foodexercise"));
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
