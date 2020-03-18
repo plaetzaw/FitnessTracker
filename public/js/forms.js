a55d83572b01a5e4a689a864bc216e6140d5e2e2
Daniela <daniela.arroyo27@gmail.com>
2020-03-18 13:21:59 -0500

    js button start

---
+++ public/js/forms.js
@@ -0,0 +1,8 @@
+var loginButton = document.getElementById('loginButton');
+loginButton.addEventListener('click', (e) => {
+    console.log('you clicked me')
+})
+// var div = document.getElementById('1');
+// div.addEventListener('click', function(e){
+//   console.log(e);
+// })
