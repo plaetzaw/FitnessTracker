9d0ecdb5d460cb1a04906166a57886585036a9c4
Daniela <daniela.arroyo27@gmail.com>
2020-03-18 21:55:21 -0500

    register

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-let db = require('../models')
+let db = require('../models');
+let bcrypt = require('bcrypt');
+let SALT_ROUNDS = 10;
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,14 +15,42 @@ router.post('/register', (req, res) => {
 
     let email = req.body.email;
     let password = req.body.password;
+    let bmr = 0;
+    let weight = 0;
 
-    db.users.create({ email: email, password: password })
-        .then(user => {
-            console.log(user)
-            res.send('received post')
-        })
-        .catch(error => {
-            console.log(error)
+    // db.users.create({ email: email, password: password, bmr: bmr })
+    //     .then(user => {
+    //         console.log('Creating user...')
+    //         console.log(user.bmr);
+    //         // console.log(user)
+    //         res.send('received post')
+    //     })
+    //     .catch(error => {
+    //         console.log(error)
+    //     })
+
+    db.users.findOne({
+        where: {
+            email: email
+        }
+    })
+        .then(persistedUser => {
+            if (persistedUser) {
+                res.status(500).json({ message: "Email is already in use" })
+            } else {
+                console.log("Hashing password...")
+                bcrypt.hash(password, SALT_ROUNDS)
+                    .then(hash => {
+                        let newUser = db.users.build({
+                            email: email,
+                            password: hash,
+                            bmr: bmr,
+                            weight: 0
+                        })
+
+                        newUser.save().then(() => res.redirect("/")).catch(err => console.error(err))
+                    })
+            }
         })
 })
 
