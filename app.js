beb32e3af638d95adb57f3f234e33edcdd4f7377
plaetzaw <plaetzaw@gmail.com>
2020-03-19 14:08:57 -0500

    Updated Reg form and homepage buttons

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
-let db = require('./models')
+let db = require("./models");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
-app.use(require('./routes/profilePage'))
+app.use(require("./routes/profilePage"));
 app.use(require("./routes/"));
 app.use(require("./routes/login"));
 app.use(require("./routes/register"));
 app.use(require("./routes/foodexercise"));
 app.use(require("./routes/tracker"));
 app.use(require("./routes/aboutus"));
-app.use(require("./routes/profile"));
+app.use(require("./routes/settings"));
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
--- routes/register.js
+++ routes/register.js
@@ -1,45 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-let db = require('../models');
-let bcrypt = require('bcrypt');
+const bodyParser = require("body-parser");
+let db = require("../models");
+let bcrypt = require("bcrypt");
 let SALT_ROUNDS = 10;
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
-router.get('/register', (req, res) => {
-    res.render('register.ejs');
+router.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 
-router.post('/register', (req, res) => {
-
-    let email = req.body.email;
-    let password = req.body.password;
-    let bmr = 0;
-    let weight = 0;
-    db.users.findOne({
-        where: {
-            email: email
-        }
+router.post("/register", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let bmr = 0;
+  let weight = req.body.weight;
+  let age = req.body.age;
+  let height = req.body.height;
+  let gender = req.body.gender;
+  db.users
+    .findOne({
+      where: {
+        email: email
+      }
     })
-        .then(persistedUser => {
-            if (persistedUser) {
-                res.status(500).json({ message: "Email is already in use" })
-            } else {
-                console.log("Hashing password...")
-                bcrypt.hash(password, SALT_ROUNDS)
-                    .then(hash => {
-                        let newUser = db.users.build({
-                            email: email,
-                            password: hash,
-                            bmr: bmr,
-                            weight: 0
-                        })
+    .then(persistedUser => {
+      if (persistedUser) {
+        res.status(500).json({ message: "Email is already in use" });
+      } else {
+        console.log("Hashing password...");
+        bcrypt.hash(password, SALT_ROUNDS).then(hash => {
+          let newUser = db.users.build({
+            email: email,
+            password: hash,
+            bmr: bmr,
+            weight: weight,
+            age: age,
+            height: height,
+            gender: gender
+          });
 
-                        newUser.save().then(() => res.redirect("/")).catch(err => console.error(err))
-                    })
-            }
-        })
-})
+          newUser
+            .save()
+            .then(() => res.redirect("/"))
+            .catch(err => console.error(err));
+        });
+      }
+    });
+});
 
 module.exports = router;
