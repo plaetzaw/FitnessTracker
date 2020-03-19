5cecf23ddd620d9c45f6f3dd44713c2504ca8991
plaetzaw <plaetzaw@gmail.com>
2020-03-19 10:04:33 -0500

    Set up full scaffolding

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(require("./routes/"));
 app.use(require("./routes/login"));
 app.use(require("./routes/register"));
 app.use(require("./routes/foodexercise"));
+app.use(require("./routes/tracker"));
+app.use(require("./routes/aboutus"));
+app.use(require("./routes/profile"));
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
