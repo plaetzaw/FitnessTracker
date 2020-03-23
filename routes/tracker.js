2c9d4642b9653d1931032246b6f211f5b1569091
plaetzaw <plaetzaw@gmail.com>
2020-03-23 14:02:00 -0500

    Updating Tracker Page

--- routes/tracker.js
+++ routes/tracker.js
@@ -1,7 +1,55 @@
 const express = require("express");
 const router = express.Router();
+let db = require("../models");
 
 router.get("/tracker", (req, res) => {
+  let bmr = req.session.bmr;
+  let weight = req.session.weight;
+  let exercise = "";
+  let calories = "";
+  db.userExercise
+    .findAll({
+      where: {
+        userid: req.session.userid
+      }
+    })
+    .then(exercises => {
+      exercises.forEach(exercise => {
+        req.session.caloriesBurned += exercise;
+      });
+    })
+    .then(() => {
+      db.calorieIntakes.findAll({
+        where: {
+          userid: req.session.userid
+        }
+      });
+    })
+    .then(foods => {
+      foods.forEach(calories => {
+        req.session.calories += calories;
+      });
+    });
+
+  let DailyWeightLoss = bmr + exercise - calories;
+
+  let WeeklyChange = DailyWeightLoss * 7;
+
+  let WeeklyWeightLoss = WeeklyChange / 3500;
+
+  let ProjectedWeeklyWeight = weight - WeeklyWeightLoss;
+
+  let MonthlyWeightLoss = (WeeklyChange * 4) / 3500;
+
+  let ProjectedMonthlyWeight = weight - MonthlyWeightLoss;
+
+  console.log(DailyWeightLoss);
+  console.log(WeeklyChange);
+  console.log(WeeklyWeightLoss);
+  console.log(MonthlyWeightLoss);
+  console.log(ProjectedWeeklyWeight);
+  console.log(ProjectedMonthlyWeight);
+
   res.render("tracker.ejs");
 });
 
