a147f5f6bb331ef6979b298066f542672de07378
plaetzaw <plaetzaw@gmail.com>
2020-03-24 10:48:23 -0500

    Tracker NOT working

--- routes/tracker.js
+++ routes/tracker.js
@@ -5,8 +5,9 @@ let db = require("../models");
 router.get("/tracker", (req, res) => {
   let bmr = req.session.bmr;
   let weight = req.session.weight;
-  let exercise = "";
-  let calories = "";
+  let userExerciseData = [];
+  let userCaloriesData = [];
+
   db.userExercise
     .findAll({
       where: {
@@ -14,43 +15,104 @@ router.get("/tracker", (req, res) => {
       }
     })
     .then(exercises => {
+      console.log("===FOUND EXERCISES===")
       exercises.forEach(exercise => {
         req.session.caloriesBurned += exercise;
+        userExerciseData.push(exercise);
       });
     })
     .then(() => {
+      console.log("===LOOKING FOR CALORIES===")
       db.calorieIntakes.findAll({
         where: {
           userid: req.session.userid
         }
-      });
+      })
+      .then(foods => {
+        console.log("===FOUND CALORIES===")
+        foods.forEach(calories => {
+          req.session.calories += calories;
+          userCaloriesData.push(calories);
+  
+          let DailyWeightLoss = bmr + userExerciseData - userCaloriesData;
+  
+          let WeeklyChange = DailyWeightLoss * 7;
+  
+          let WeeklyWeightLoss = WeeklyChange / 3500;
+  
+          let ProjectedWeeklyWeight = weight - WeeklyWeightLoss;
+  
+          let MonthlyWeightLoss = (WeeklyChange * 4) / 3500;
+  
+          let ProjectedMonthlyWeight = weight - MonthlyWeightLoss;
+  
+          let TwoMonthLoss = weight - 2 * MonthlyWeightLoss;
+  
+          let ThreeMonthLoss = weight - 3 * MonthlyWeightLoss;
+  
+          let FourMonthLoss = weight - 4 * MonthlyWeightLoss;
+          console.log("THESE ARE THE VARIABLES")
+          console.log(DailyWeightLoss);
+          console.log(WeeklyChange);
+          console.log(WeeklyWeightLoss);
+          console.log(MonthlyWeightLoss);
+          console.log(ProjectedWeeklyWeight);
+          console.log(ProjectedMonthlyWeight);
+          console.log(TwoMonthLoss);
+          console.log(ThreeMonthLoss);
+          console.log(FourMonthLoss);
+  
+          window.onload = function() {
+            var month = new Array();
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "November";
+            month[11] = "December";
+            var d = new Date();
+            const n = month[d.getMonth()];
+            const o = month[d.getMonth() + 1];
+            const p = month[d.getMonth() + 2];
+            const q = month[d.getMonth() + 3];
+  
+            const CHART = document.getElementById("lineChart");
+            console.log(CHART);
+            let lineChart = new Chart(CHART, {
+              type: "line",
+              data: {
+                labels: [n, o, p, q],
+                datasets: [
+                  {
+                    label: "2020",
+                    data: [
+                      ProjectedMonthlyWeight,
+                      TwoMonthLoss,
+                      ThreeMonthLoss,
+                      FourMonthLoss
+                    ]
+                  }
+                ]
+              }
+            });
+          };
+        });
+      })
+      .then(() => {
+        console.log("===DONE===")
+        res.render("tracker.ejs");
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).json({error: err})
+      })
     })
-    .then(foods => {
-      foods.forEach(calories => {
-        req.session.calories += calories;
-      });
-    });
-
-  let DailyWeightLoss = bmr + exercise - calories;
-
-  let WeeklyChange = DailyWeightLoss * 7;
-
-  let WeeklyWeightLoss = WeeklyChange / 3500;
-
-  let ProjectedWeeklyWeight = weight - WeeklyWeightLoss;
-
-  let MonthlyWeightLoss = (WeeklyChange * 4) / 3500;
-
-  let ProjectedMonthlyWeight = weight - MonthlyWeightLoss;
-
-  console.log(DailyWeightLoss);
-  console.log(WeeklyChange);
-  console.log(WeeklyWeightLoss);
-  console.log(MonthlyWeightLoss);
-  console.log(ProjectedWeeklyWeight);
-  console.log(ProjectedMonthlyWeight);
-
-  res.render("tracker.ejs");
 });
 
 module.exports = router;
