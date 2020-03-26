0b77a49ba1a4281dee65ff65fe440dfe5e79c823
Matt Ryan <matthewtryan99@gmail.com>
2020-03-26 00:57:55 -0500

    homepage styling

--- routes/chat.js
+++ routes/chat.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/chat', (req, res) => {
-    res.render('chat')
+    res.render('chat', {
+        userID: req.session.userid
+    })
 })
 
 module.exports = router
